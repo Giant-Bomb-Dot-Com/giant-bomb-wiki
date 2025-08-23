0ecef5649e2460b87ee3c7145b05f2ada6944337
Steven Ng <stng98@gmail.com>
2025-08-22 18:46:37 -0700

    move templates over from properties

---
+++ gb_api_scripts/generate_xml_templates.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once(__DIR__.'/common.php');
+
+class GenerateXMLTemplates extends Maintenance
+{
+    use CommonVariablesAndMethods;
+
+    public function __construct() 
+    {
+        parent::__construct();
+        $this->addDescription("Generates XML for templates");
+    }
+
+    public function execute()
+    {
+        $data = [
+            [
+                'title' => 'Template:Rating',
+                'namespace' => $this->namespaces['template'],
+                'description' => <<<MARKUP
+<noinclude>
+{{#template_params:Name (property=Has name)|Explanation (property=Stands for)|Image (property=Has image)|Description (property=Has description)}}
+</noinclude><includeonly>
+'''Name:''' [[Has name::{{{Name|}}}]]
+
+'''Explanation:''' [[Stands for::{{{Explanation|}}}]]
+
+'''Image:''' [[Has image::{{{Image|}}}]]
+
+'''Description:''' [[Has description::{{{Description|}}}]]
+
+''':''' {{#ask:[[Foaf:homepage::{{SUBJECTPAGENAME}}]]|format=list}}
+</includeonly>
+MARKUP;
+            ],
+            [
+                'title' => 'Template:CreateWithForm',
+                'namespace' => $this->namespace['template'],
+                'description' => '<includeonly>
+{{#formlink:
+  form={{{1}}}
+| link text=Create with form
+| target={{PAGENAME}}
+}}
+</includeonly>'
+            ],
+        ];
+
+        $this->createXML('templates.xml', $data);
+    }
+}
+
+$maintClass = GenerateXMLTemplates::class;
+
+require_once RUN_MAINTENANCE_IF_MAIN; 
