c15f4ee194e930c91dd7e61a3ae20efab2774a72
Steven Ng <stng98@gmail.com>
2025-08-25 14:25:16 -0700

    give db api time to setup before health check

--- gb_api_db_init/20_wiki_schema_add_page_name_column.sql
+++ gb_api_db_init/20_wiki_schema_add_page_name_column.sql
@@ -3,8 +3,24 @@
 --
 USE gb_api_dump;
 
+UPDATE `wiki_game_dlc` SET `mw_formatted_description` = NULL;  
+UPDATE `wiki_franchise` SET `mw_formatted_description` = NULL;                 
+UPDATE `wiki_game_release` SET `mw_formatted_description` = NULL;              
+UPDATE `wiki_game_theme` SET `mw_formatted_description` = NULL;                
+UPDATE `wiki_accessory` SET `mw_formatted_description` = NULL;                 
+UPDATE `wiki_platform` SET `mw_formatted_description` = NULL;                  
+UPDATE `wiki_location` SET `mw_formatted_description` = NULL;                  
+UPDATE `wiki_character` SET `mw_formatted_description` = NULL;                 
+UPDATE `wiki_thing` SET `mw_formatted_description` = NULL;                     
+UPDATE `wiki_game` SET `mw_formatted_description` = NULL;                      
+UPDATE `wiki_game_genre` SET `mw_formatted_description` = NULL;                
+UPDATE `wiki_person` SET `mw_formatted_description` = NULL;                    
+UPDATE `wiki_company` SET `mw_formatted_description` = NULL;                   
+UPDATE `wiki_concept` SET `mw_formatted_description` = NULL;                   
+-- add mw_page_num column
 ALTER TABLE `wiki_game_dlc` ADD COLUMN `mw_page_name` varchar(255)  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL;                       
 ALTER TABLE `wiki_franchise` ADD COLUMN `mw_page_name` varchar(255)  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL;                      
+ALTER TABLE `wiki_game_release` ADD COLUMN `mw_page_name` varchar(255)  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL;                     
 ALTER TABLE `wiki_game_theme` ADD COLUMN `mw_page_name` varchar(255)  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL;                     
 ALTER TABLE `wiki_accessory` ADD COLUMN `mw_page_name` varchar(255)  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL;                      
 ALTER TABLE `wiki_platform` ADD COLUMN `mw_page_name` varchar(255)  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL;                       
