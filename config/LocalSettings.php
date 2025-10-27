5bd2247532962faa691eeba046c27a3a6b3d3005
secretrob <magus91@gmail.com>
2025-10-27 09:46:00 -0500

    Changed back to giantbomb skin

--- config/LocalSettings.php
+++ config/LocalSettings.php
@@ -269,7 +269,7 @@ $wgGroupPermissions["*"]["edit"] = false;
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
-$wgDefaultSkin = "vector";
+$wgDefaultSkin = "giantbomb";
 
 # Enabled skins.
 # The following skins were automatically enabled:
