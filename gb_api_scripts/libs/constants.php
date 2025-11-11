c7f633fcf715fc370c6a7d7ac6956569fe465d7f
songknny <songknny@gmail.com>
2025-11-11 14:53:44 -0800

    Use the full URL in imagelink

---
+++ gb_api_scripts/libs/constants.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Giant Bomb API Association Type IDs
+ * Extracted from trait to support PHP 8.1 (traits can't have constants until PHP 8.2)
+ */
+interface GiantBombConstants
+{
+    const ASSOC_TYPE_ACCESSORY = 3000;
+    const ASSOC_TYPE_CHARACTER = 3005;
+    const ASSOC_TYPE_COMPANY = 3010;
+    const ASSOC_TYPE_CONCEPT = 3015;
+    const ASSOC_TYPE_DLC = 3020;
+    const ASSOC_TYPE_FRANCHISE = 3025;
+    const ASSOC_TYPE_GAME = 3030;
+    const ASSOC_TYPE_THEME = 3032;
+    const ASSOC_TYPE_LOCATION = 3035;
+    const ASSOC_TYPE_PERSON = 3040;
+    const ASSOC_TYPE_PLATFORM = 3045;
+    const ASSOC_TYPE_RELEASE = 3050;
+    const ASSOC_TYPE_THING = 3055;
+    const ASSOC_TYPE_GENRE = 3060;
+    const ASSOC_TYPE_GAME_RATING = 3065;
+    const ASSOC_TYPE_RATING_BOARD = 3070;
+    const ASSOC_TYPE_REGION = 3075;
+}
--- gb_api_scripts/libs/converter.php
+++ gb_api_scripts/libs/converter.php
@@ -428,10 +428,10 @@ class HtmlToMediaWikiConverter
     }
 
     /**
-     * Converts <figure> to MediaWiki image syntax. External image links is just the url.
+     * Converts <figure> to MediaWiki image syntax using full URL.
      *
      * @param DOMElement  $figure The <figure> element to convert.
-     * @return string|false The MediaWiki formatted image string.
+     * @return string|false The MediaWiki formatted image string with full URL.
      */
     public function convertFigure(DOMElement $figure): string|false
     {
@@ -461,11 +461,8 @@ class HtmlToMediaWikiConverter
             $float = 'none';
         }
 
-        $imageFragment = parse_url($src, PHP_URL_PATH);
-        $imageFile = basename($imageFragment);
-        $imageFile = str_replace($this->reservedCharacters, ' ', $imageFile);
-
-        $mwImage = "[[File:{$imageFile}|thumb|{$float}";
+        // Use full URL in File syntax instead of just filename
+        $mwImage = "[[File:{$src}|thumb|{$float}";
         if ($alt != 'No Caption Provided') {
             $mwImage .= "|alt={$alt}|{$alt}";
         }
@@ -492,18 +489,15 @@ class HtmlToMediaWikiConverter
     }
 
     /**
-     * Converts <image> to MediaWiki image syntax. External image links is just the url.
+     * Converts <image> to MediaWiki image syntax using full URL.
      *
      * @param DOMElement  $image The <image> element to convert.
-     * @return string The MediaWiki formatted image string.
+     * @return string The MediaWiki formatted image string with full URL.
      */
     public function convertImage(DOMElement $image): string|false
     {
         $src = $image->getAttribute('data-img-src');
         $align = $image->getAttribute('data-align');
-        $imageFragment = parse_url($src, PHP_URL_PATH);
-        $imageFile = basename($imageFragment);
-        $imageFile = str_replace($this->reservedCharacters, ' ', $imageFile);
 
         if ($align == 'right') {
             $float = 'right';
@@ -515,7 +509,8 @@ class HtmlToMediaWikiConverter
             $float = 'none';
         }
 
-        $mwImage = "[[File:{$imageFile}|thumb|{$float}]]";
+        // Use full URL in File syntax instead of just filename
+        $mwImage = "[[File:{$src}|thumb|{$float}]]";
 
         return $mwImage;
     }
