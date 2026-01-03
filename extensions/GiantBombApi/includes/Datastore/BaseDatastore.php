95b6eb22e672a313c6743aa589f07a96fa786659
Michael Enger <git@michaelenger.com>
2026-01-04 00:04:32 +0100

    Make BaseDatastore parent class to handle the querying

---
+++ extensions/GiantBombApi/includes/Datastore/BaseDatastore.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace GiantBombApi\Datastore;
+
+use Exception;
+use GiantBombApi\Helpers\PageHelper;
+use SMW;
+use SMWQueryProcessor;
+
+/**
+ * Datastore connection for retrieving pages from SMW and massaging the data to
+ * be API-friendly.
+ */
+class BaseDatastore {
+
+    /**
+     * Get a list of resources based off a category of pages and their properties.
+     * @param string $category Title of the category to fetch pages for.
+     * @param array $properties List of properties to extract from the pages.
+     * @param SortOrder $sortOrder Order of the pages.
+     * @param int $limit Amount of pages to fetch.
+     * @param int $offset Page count offset.
+     * @return array Array containing the list of results and the total number of pages.
+     */
+    protected static function getResources( string $category, array $properties, SortOrder $sortOrder, int $limit, int $offset ): array {
+        $store = SMW\StoreFactory::getStore(); // TODO: is this a MW service that can be dependency injected?
+
+        $queryConditions = '[[Category:' . $category . ']]';
+        $queryParams = [
+            $queryConditions,
+            'limit=' . $limit,
+            'offset=' . $offset,
+        ];
+        foreach ($properties as $property) {
+            $queryParams[] = '?Has ' . $property;
+        }
+
+        // Apply sorting
+        switch ($sortOrder) {
+            case SortOrder::Default:
+                // do nothing
+                break;
+            case SortOrder::NameAsc:
+                $queryParams[] = 'sort=Has name';
+                $queryParams[] = 'order=asc';
+                break;
+            case SortOrder::NameDesc:
+                $queryParams[] = 'sort=Has name';
+                $queryParams[] = 'order=desc';
+                break;
+            default:
+                // TODO: more graceful failing
+                throw new Exception('Unhandled sort order: ' . $sortOrder->value);
+        }
+
+        // Setup query
+        list($queryString, $params, $printouts) = SMWQueryProcessor::getComponentsFromFunctionParams(
+            $queryParams,
+            false
+        );
+        $query = SMWQueryProcessor::createQuery(
+            $queryString,
+            SMWQueryProcessor::getProcessedParams($params),
+            SMWQueryProcessor::INLINE_QUERY,
+            '',
+            $printouts
+        );
+
+        // Fetch query results
+        $queryResult = $store->getQueryResult($query);
+        $results = [];
+        while ($row = $queryResult->getNext()) {
+            $subject = $row[0]->getResultSubject();
+            $title = $subject->getTitle();
+
+            $pageData = [
+                'id' => $subject->getSerialization(),
+                'url' => '/wiki/' . $title->getPrefixedDBkey(),
+            ];
+
+            foreach ($properties as $property) {
+                $name = self::sanitizePropertyName($property);
+                $pageData[$name] = null;
+            }
+
+            // Extract property values
+            foreach ($row as $field) {
+                $label = $field->getPrintRequest()->getLabel();
+                $values = [];
+                while ($dataValue = $field->getNextDataValue()) {
+                    $values[] = $dataValue->getShortWikiText();
+                }
+
+                $name = self::sanitizePropertyName(
+                    substr($label, 4) // remove the "Has " prefix
+                );
+                $pageData[$name] = self::parseProperty($label, $values);
+            }
+
+            $results[] = $pageData;
+        }
+
+        return [
+            $results,
+            123, // TODO: total results
+        ];
+    }
+
+    /**
+     * Parse a page property values into an API-friendly format.
+     * @param string $label Label of the property.
+     * @param array $values The values stored in the property.
+     * @return string|array|null API-friendly version of the property.
+     */
+    protected static function parseProperty( string $label, array $values): string|array|null {
+        switch ($label) {
+            case 'Has guid':
+            case 'Has deck':
+            case 'Has image':
+            case 'Has name':
+                return $values[0] ?? '';
+                break;
+
+            case 'Has platforms':
+                return array_map(
+                    fn ($p) => PageHelper::humanizeTitle($p),
+                    $values
+                );
+                break;
+
+            case 'Has release date':
+                if (!empty($values[0])) {
+                    $timestamp = strtotime($values[0]);
+                    if ($timestamp !== false) {
+                        return date('Y-m-d', $timestamp);
+                    }
+                }
+                break;
+
+            default:
+                // TODO: more graceful failing
+                throw new Exception('Unhandled label: ' . $label);
+        }
+
+        return null;
+    }
+
+    /**
+     * Convert a property name to a keyword-style name, removing spaces and
+     * making lowercase.
+     * @param string $property Property name to sanitize.
+     * @return string A sanitized name.
+     */
+    protected static function sanitizePropertyName( string $property ): string {
+        return strtolower(str_replace(' ', '_', $property));
+    }
+
+}
--- extensions/GiantBombApi/includes/Datastore/GamesDatastore.php
+++ extensions/GiantBombApi/includes/Datastore/GamesDatastore.php
@@ -2,137 +2,32 @@
 
 namespace GiantBombApi\Datastore;
 
-use Exception;
-use GiantBombApi\Helpers\PageHelper;
-use SMW;
-use SMWQueryProcessor;
-
 /**
  * Datastore connection for retrieving games.
  */
-class GamesDatastore {
+class GamesDatastore extends BaseDatastore {
 
     /**
      * Get a list of games.
      * @param SortOrder $sortOrder Sort order of the results.
      * @param int $limit How many results to fetch.
      * @param int $offset Result offset.
-     * @return array List of filtered, limited & sorted games, along with the total games.
+     * @return array List of filtered, limited & sorted games, along with the total available games.
      */
     public static function getGames( SortOrder $sortOrder, int $limit, int $offset ): array {
-        $store = SMW\StoreFactory::getStore();
-
-        $queryConditions = '[[Category:Games]]';
-        $queryParams = [
-            $queryConditions,
-            'limit=' . $limit,
-            'offset=' . $offset,
-            '?Has guid',
-            '?Has name',
-            '?Has deck',
-            '?Has image',
-            '?Has release date',
-            '?Has platforms'
-        ];
-
-        switch ($sortOrder) {
-            case SortOrder::Default:
-                // do nothing
-                break;
-            case SortOrder::NameAsc:
-                $queryParams[] = 'sort=Has name';
-                $queryParams[] = 'order=asc';
-                break;
-            case SortOrder::NameDesc:
-                $queryParams[] = 'sort=Has name';
-                $queryParams[] = 'order=desc';
-                break;
-            default:
-                // TODO: more graceful failing
-                throw new Exception('Unhandled sort order: ' . $sortOrder->value);
-        }
-
-        list($queryString, $params, $printouts) = SMWQueryProcessor::getComponentsFromFunctionParams(
-            $queryParams,
-            false
+        return self::getResources(
+            'Games',
+            [
+                'guid',
+                'name',
+                'deck',
+                'image',
+                'release date',
+                'platforms',
+            ],
+            $sortOrder,
+            $limit,
+            $offset
         );
-
-        $query = SMWQueryProcessor::createQuery(
-            $queryString,
-            SMWQueryProcessor::getProcessedParams($params),
-            SMWQueryProcessor::INLINE_QUERY,
-            '',
-            $printouts
-        );
-
-        $queryResult = $store->getQueryResult($query);
-        $results = [];
-        while ($row = $queryResult->getNext()) {
-            $subject = $row[0]->getResultSubject();
-            $title = $subject->getTitle();
-
-            $pageData = [
-                'id' => $subject->getSerialization(),
-                'guid' => null,
-                'url' => '/wiki/' . $title->getPrefixedDBkey(),
-                'deck' => null,
-                'name' => PageHelper::humanizeTitle($title->getText()),
-                'description' => null,
-                'image' => null,
-                'original_release_date' => null,
-                'platforms' => null,
-            ];
-
-            // Extract property values
-            for ($i = 0; $i < count($row); $i++) {
-                $field = $row[$i];
-                $label = $field->getPrintRequest()->getLabel();
-
-                $values = [];
-                $dv = null;
-                while ($tempDV = $field->getNextDataValue()) {
-                    $dv = $tempDV;
-                    $values[] = $dv->getShortWikiText();
-                }
-
-                switch ($label) {
-                    case 'Has guid':
-                        $pageData['guid'] = $values[0] ?? '';
-                        break;
-                    case 'Has name':
-                        $pageData['name'] = $values[0] ?? $pageData['name'];
-                        break;
-                    case 'Has deck':
-                        $pageData['deck'] = $values[0] ?? '';
-                        break;
-                    case 'Has image':
-                        $pageData['image']= $values[0] ?? '';
-                        break;
-                    case 'Has release date':
-                        if (!empty($values[0])) {
-                            $timestamp = strtotime($values[0]);
-                            if ($timestamp !== false) {
-                                $pageData['original_release_date'] = date('Y-m-d', $timestamp);
-                            }
-                        }
-                        break;
-                    case 'Has platforms':
-                        $pageData['platforms'] = array_map(function($p) {
-                            return PageHelper::humanizeTitle($p);
-                        }, $values);
-                        break;
-                    default:
-                        // TODO: more graceful failing
-                        throw new Exception('Unhandled label: ' . $label);
-                }
-            }
-
-            $results[] = $pageData;
-        }
-
-        return [
-            $results,
-            123, // TODO: total results
-        ];
     }
 }
